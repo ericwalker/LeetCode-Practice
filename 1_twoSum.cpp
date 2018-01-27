4f8ebf66868049e9429b13b1ca96d5dae2f4e709
ericwalker <ericdavid78929@gmail.com>
2018-01-28 00:08:25 +0800

    practice twoSum and addTwoNumbers

---
+++ 1_twoSum.cpp
@@ -0,0 +1,26 @@
+//LeetCode, Two Sum
+// Use hash
+// Space Complexity: O(n), Time Complexity: O(n)
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) 
+    {
+        unordered_map<int, int> my_map;
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            my_map[nums[i]] = i; // save the key value into the unordered map
+        }
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            const int gap = target - nums[i];
+            if (my_map.find(gap) != my_map.end() && my_map[gap] > i) 
+            {
+                result.push_back(i);
+                result.push_back(my_map[gap]);
+                break;
+            }
+        }
+        return result;
+    }
+};
---
+++ linkedList.cpp
@@ -0,0 +1,10 @@
+include <iostream>
+using namespace std;
+
+struct List
+{
+	char *name;
+	int age;
+	List *next;	
+};
+
