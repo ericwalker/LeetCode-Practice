65ef7caeba0cba0656aa6917eb69c063adbc4720
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-11-22 09:56:47 +0900

    complete 344_reverse_string practice

--- 344_Reverse_String.java
+++ 344_Reverse_String.java
@@ -3,13 +3,16 @@ import java.util.*;
 class Solution {
     public void reverseString(char[] s) {
 		
-		
-		for (int i=0;i<s.length;i++)	
-		{}
+		// int left = 0;
+		int right = s.length-1;
+		char temp;
 
-
+		for (int left=0;left<s.length/2;left++)	
+		{
+			temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+			right -= 1; 
+		}
 	}
-	
-	// public int helper ()
-
 }
