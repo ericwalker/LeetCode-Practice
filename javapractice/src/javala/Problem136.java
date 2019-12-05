5b8e196141e5827a2ba73e53b8902b26bb64f809
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-12-05 14:42:07 +0900

    finished Problem 136

--- javapractice/src/javala/Problem136.java
+++ javapractice/src/javala/Problem136.java
@@ -1,4 +1,27 @@
+// 136. Single Number
 package javala;
 
-public class Problem136 {
+// we use bitwise XOR
+
+// N1 ^ N1 ^ N2 ^ N2 ^..............^ Nx ^ Nx ^ N
+// = (N1^N1) ^ (N2^N2) ^..............^ (Nx^Nx) ^ N
+// = 0 ^ 0 ^ ..........^ 0 ^ N
+// = N
+
+class Problem136 {
+    public int singleNumber(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        return helper(nums);
+    }
+
+    public int helper (int[] nums)
+    {
+        int N = nums[0];
+        for (int i = 1; i<nums.length;i++)
+        {
+            N ^= nums[i];
+        }
+        return N;
+    }
 }
