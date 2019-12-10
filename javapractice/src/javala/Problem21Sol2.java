f71953318d2f2c17b716a80e1cbf93bdc22dd590
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-12-10 09:59:27 +0900

    finished Problem 21

--- javapractice/src/javala/Problem21Sol2.java
+++ javapractice/src/javala/Problem21Sol2.java
@@ -2,5 +2,42 @@
 // 21. Merge Two Sorted Lists
 package javala;
 
-public class Problem21Sol2 {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Problem21Sol2 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode result = new ListNode(0);
+        ListNode p = result;
+
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        if (l1 != null || l2 != null)
+        {
+            if (l1.val <= l2.val)
+            {
+                result.val = l1.val;
+                result.next = mergeTwoLists(l1.next,l2);
+            }
+
+            else if (l1.val > l2.val)
+            {
+                result.val = l2.val;
+                result.next = mergeTwoLists(l1,l2.next);
+            }
+        }
+
+
+
+        return result;
+    }
 }
