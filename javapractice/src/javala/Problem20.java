fad3eae6ab0c8bb0a4015b4bbe45fb87454bf4e9
ericwalker <ericdavid78929@gmail.com>
2019-12-10 22:34:38 +0900

    finished problem20

---
+++ javapractice/src/javala/Problem20.java
@@ -0,0 +1,46 @@
+package javala
+
+class Problem20 {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<Character>();
+        boolean isFormalParentheses = true;
+
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '(')
+                stack.push(s.charAt(i));
+            else
+            {
+                isFormalParentheses = parenthesesChecker(s.charAt(i), stack);
+                if (!isFormalParentheses)
+                    return false;
+            }
+        }
+
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    public boolean parenthesesChecker (Character c, Stack<Character> stack)
+    {
+        // switch:
+        //     case 1:
+        if (stack.isEmpty())
+            return false;
+        if (c == ')' && stack.peek() == '(')
+            stack.pop();
+        else if (c == ']' && stack.peek() == '[')
+            stack.pop();
+        else if (c == '}' && stack.peek() == '{')
+            stack.pop();
+        else
+            return false;
+
+        return true;
+    }
+
+
+}
