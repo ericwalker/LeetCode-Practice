922f0e402213b6d9ae055ee23fe2cf044f841276
ericwalker <ericdavid78929@gmail.com>
2019-12-10 21:48:30 +0900

    finished problem94

--- javapractice/src/javala/Problem94Sol2.java
+++ javapractice/src/javala/Problem94Sol2.java
@@ -1,5 +1,36 @@
 package javala;
 
-public class Problem94Sol2 {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Problem94Sol2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
 
+        List<Integer> resultArray = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        TreeNode curNode = root;
+
+        while (curNode != null || !stack.isEmpty())
+        {
+            while (curNode != null)
+            {
+                stack.push(curNode);
+                curNode = curNode.left;
+            }
+
+            curNode = stack.pop();
+            resultArray.add(curNode.val);
+
+            curNode = curNode.right;
+        }
+
+        return resultArray;
+    }
 }
