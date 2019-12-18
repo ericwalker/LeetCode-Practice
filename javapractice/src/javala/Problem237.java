24bd4ccff2889982b83793bfbaee71aac910e162
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-12-18 14:06:12 +0900

    finished Problem237 (not a good question)

---
+++ javapractice/src/javala/Problem237.java
@@ -0,0 +1,17 @@
+// Stupid Question
+package javala;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Problem237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
