84a9433cbef3579b8df2a51f31d483fb6f3c7125
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-12-19 14:43:52 +0900

    finished Problem705

---
+++ javapractice/src/javala/Problem705.java
@@ -0,0 +1,30 @@
+package javala;
+
+class MyHashSet {
+    boolean[] map = new boolean[1000001];
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+    }
+
+    public void add(int key) {
+        map[key] = true;
+    }
+
+    public void remove(int key) {
+        map[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return map[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
--- playground/src/HelloWorld.java
+++ playground/src/HelloWorld.java
@@ -4,40 +4,36 @@ import java.util.Arrays;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println(countPrimes(100));
+
+    public static class MyHashSet {
+
+        boolean[] map = new boolean[1000000];
+
+        /** Initialize your data structure here. */
+        public MyHashSet() {
+
+        }
+
+        public void add(int key) {
+
+            map[key] = true;
+        }
+
+        public void remove(int key) {
+            map[key] = false;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public boolean contains(int key) {
+            return map[key];
+        }
     }
 
-    public static int countPrimes(int n) {
 
-        if (n <= 2)
-            return 0;
-        else if (n == 3)
-            return 1;
-
-        List<Integer> primeArray = new ArrayList<Integer>();
-        primeArray.add(2);
-        primeArray.add(3);
-
-        for (int i = 4; i <= n-1; i++)
-        {
-            // judge if i is prime or not
-            if ((i+1)%6 == 0 || (i-1)%6 == 0)
-            {
-                boolean isPrime = true;
-
-                for (int primeNum : primeArray) {
-                    if (i % primeNum == 0) {
-                        isPrime = false;
-                        break;
-                    }
-                }
-                if (isPrime)
-                {
-                    primeArray.add(i);
-                }
-            }
-        }
-        return primeArray.size();
+    public static void main(String[] args) {
+        MyHashSet obj = new MyHashSet();
+        obj.add(1);
+        obj.remove(2);
+        boolean param_3 = obj.contains(3);
     }
 }
