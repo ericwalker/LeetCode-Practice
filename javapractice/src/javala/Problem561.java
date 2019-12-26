b2b1b63842378ba4e9cb636be73fbfb283e894bd
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2019-12-26 15:06:57 +0900

    finished Problem38 & Problem561

--- javapractice/src/javala/Problem561.java
+++ javapractice/src/javala/Problem561.java
@@ -1,4 +1,12 @@
-package javala;
+class Solution {
+    public int arrayPairSum(int[] nums) {
 
-public class Problem561 {
+        Arrays.sort(nums);
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i += 2)
+            result = result + nums[i];
+
+        return result;
+    }
 }
--- playground/src/HelloWorld.java
+++ playground/src/HelloWorld.java
@@ -6,19 +6,6 @@ import java.util.Arrays;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        String haystack = "hello";
-        String needle = "ll";
 
-        if (needle.length() == 0)
-            System.out.println(0);
-
-        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
-            if (needle.equals(haystack.substring(i, i + needle.length()))) {
-                System.out.println(i);
-                break;
-            }
-        }
-
-        System.out.println(-1);
     }
 }
