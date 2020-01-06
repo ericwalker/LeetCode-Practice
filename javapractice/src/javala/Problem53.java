6f347d6ac5dff83c594a6ece8f8414817bee5a46
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2020-01-06 17:12:29 +0900

    finished Problem53 in greedy algo

--- javapractice/src/javala/Problem53.java
+++ javapractice/src/javala/Problem53.java
@@ -1,4 +1,18 @@
-package javala;
+class Solution {
+    public int maxSubArray(int[] nums) {
 
-public class Problem53 {
+        int maxSum = nums[0];
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) // modified to one layer
+        {
+            sum = sum + nums[i];
+            if (sum > maxSum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return maxSum;
+    }
 }
