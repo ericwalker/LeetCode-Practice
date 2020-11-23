43bf7b6253768f3b87ba11ffecf6bffa34bcc041
Lin, Shihchun | Eric | RP <shihchun.lin@rakuten.com>
2020-11-23 21:06:22 +0900

    finish q 173

---
+++ VSCodeLeetcode/173.binary-search-tree-iterator.java
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=173 lang=java
+ *
+ * [173] Binary Search Tree Iterator
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+    
+    public BSTIterator(TreeNode root) {    
+        stack = new Stack<>();
+        fillStack(root); // call fillStack in constructor before any next() or hasNext is called.
+    }
+    
+    public void fillStack (TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curNode = stack.pop();
+        fillStack(curNode.right);
+        
+        return curNode.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+// @lc code=end
+
